a582602dcdf1735f3e52b3d46cf69831d906d06f
Aadesh Gupta <aadeshgupta00@gmail.com>
2025-06-03 21:51:02 +0530

    add more variables

--- src/config.py
+++ src/config.py
@@ -4,4 +4,5 @@ import os
 root_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 data_dir = os.path.join(root_dir, 'data')
 alexa_dir = os.path.join(data_dir, 'alexa_reviews')
+models_dir = os.path.join(data_dir, 'models')
 tsv_filepath = os.path.join(alexa_dir, 'amazon_alexa.tsv')
